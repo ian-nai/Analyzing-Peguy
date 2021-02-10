18d0e4b9af620680e276ebff643b9eda3020e6a5
Ian <iangoodale@utexas.edu>
2021-02-10 12:04:18 -0600

    Update line_tokenize_remove_stopwords.py

--- python_code/line_tokenize_remove_stopwords.py
+++ python_code/line_tokenize_remove_stopwords.py
@@ -3,78 +3,68 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import LineTokenizer
 import re
+import string
+
 
 index_num = 0
 
 # NLTK's default French stopwords
 default_stopwords = set(nltk.corpus.stopwords.words('french'))
 
-input_files = ['la_tapisserie_de_sainte_genevieve.txt',
+
+input_files = ['la_tapisserie_de_sainte_genevieve.txt']
+'''
+,
 'la_tapisserie_notre_dame.txt',
 'le_mystere_de_la_charite_de_jeanne_darc.txt',
 'le_mystere_des_saints_innocents.txt',
 'le_porche_du_mystere_de_la_deuxieme_vertu.txt',
-'eve.txt']
-
+'peguy_eve.txt']
+'''
 for f in input_files:
     fp = codecs.open(f, 'r', 'utf-8')
+    text = fp.read()
+    lines = LineTokenizer(blanklines='discard').tokenize(text)
 
+    final_lines = []
 
-    t = fp.read()
-
-    new_lines = []
-
-    lines_split = LineTokenizer(blanklines='discard').tokenize(t)
-    print(lines_split)
-
-    for line in lines_split:
+    for line in lines:
+# Replace the target string
         words = nltk.word_tokenize(line)
 
     # remove punctuation
- #    from nltk.tokenize import RegexpTokenizer
-#     tokenizer2 = RegexpTokenizer(r'\w+')
-#     for word in words:
-#         tokenizer2.tokenize(word)
+      #   from nltk.tokenize import RegexpTokenizer
+#         tokenizer2 = RegexpTokenizer(r'\w+')
+#         for word in words:
+#             tokenizer2.tokenize(word)
 
     # Remove single-character tokens (mostly punctuation)
-   # words = [word for word in words if len(word) > 1]
+        words = [word for word in words if len(word) > 1]
 
     # Remove numbers
-    #words = [word for word in words if not word.isnumeric()]
+        words = [word for word in words if not word.isnumeric()]
 
     # Lowercase all words (default_stopwords are lowercase too)
         words = [word.lower() for word in words]
 
 
-
-        # Remove single character words/punctuation
-        words = [word for word in words if len(word) > 1]
-
-        # Remove numbers
-        words = [word for word in words if not word.isnumeric()]
-
-        # Lowercase all words (default_stopwords are lowercase too)
-        words = [word.lower() for word in words]
-
-        # Remove stopwords
+    # Remove stopwords
         words = [word for word in words if word not in default_stopwords]
 
         for word in words:
-            word = [item.replace("_", "") for item in word]
-            word = [item.replace("|", "") for item in word]
-            pattern = '[0-9]'
-            word = [re.sub(pattern, '', i) for i in word]
+            word = word.replace("_", "")
+        for word in words:
+            word = "".join(l for l in word if l not in string.punctuation)
 
+        line = ' '.join(words)
+        final_lines.append(line)
 
-        new_lines.append(str(words))
-        for line in new_lines:
-            if not line:
-                new_lines.remove(line)
+    #print(final_lines)
+        print(words)
+# Write the file out again
+    with open(('stopform_' + input_files[index_num]), 'w') as f:
+            #f.write("%s\n" % item)
+        f.write('\n'.join(final_lines))
 
-        print(new_lines)
-
-    # Save our new file as 'cleaned_text.txt'
-    with open(('lines_' + input_files[index_num]), 'w') as f:
-                f.write('\n'.join(new_lines))
 
     index_num += 1
