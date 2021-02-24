565f04363b2f2e62b0652bfc8f1cea20448c1583
Ian <iangoodale@utexas.edu>
2021-02-24 11:40:44 -0600

    Update csv_cleaner_GITHUB.py

--- python_code/csv_cleaner_GITHUB.py
+++ python_code/csv_cleaner_GITHUB.py
@@ -7,14 +7,10 @@ import csv
 
 index_num = 0
 
-files = ["la_tapisserie_de_sainte_genevieve.txt"] #, "nostop_le_mystere_des_saints_innocents.txt", "nostop_le_porche_du_mystere_de_la_deuxieme_vertu.txt", "nostop_peguy_eve.txt"]
+files = ['test1.txt', 'test2.txt']#["lines_la_tapisserie_de_sainte_genevieve.txt", "lines_le_mystere_des_saints_innocents.txt", "lines_le_porche_du_mystere_de_la_deuxieme_vertu.txt", "lines_eve.txt"]
 
 
 nlp = spacy.load("fr_dep_news_trf")
-# pos = POSTagger()
-# #french_lemmatizer = LefffLemmatizer(after_melt=True, default=True)
-# nlp.add_pipe(pos, name='pos', after='parser')
-# nlp.add_pipe(french_lemmatizer, name='lefff', after='pos')
 
 
 list_keys = []
@@ -27,10 +23,9 @@ for f in files:
     # Open our file
     with open(f) as file:
         text_data = file.read()
-    #print(text_data)
+
     testing_list = text_data.splitlines()
 
-    # print(testing_list)
     for x in testing_list:
         doc = nlp(x)
         pos_elements = []
@@ -41,16 +36,6 @@ for f in files:
             pos_elements.append(d)
             actual_pos.append(d.pos_)
 
-    # Specify the information we want
-    # for x in testing_list:
-    #
-    #     doc = nlp(x)
-    #     pos_elements = []
-    #     actual_pos = []
-    #
-    #     for q in nlp.pipe(doc):
-    #         # Do something with the doc here
-    #         print([(ent.text, ent.label_) for ent in doc.ents])
 
         pos_keys = Counter(actual_pos).keys() # equals to list(set(words))
         pos_nums = Counter(pos_elements).values() # counts the elements' frequency
@@ -77,12 +62,14 @@ for f in files:
     print(len(list_nums_unique))
 
 # Output our tagged data into a CSV
-    with open('text_testing_SPACY3.csv', 'w') as csvfile:
+    with open('pos_spacy' + files[index_num] + '.csv', 'w') as csvfile:
         fieldnames = ['text', 'pos', 'total', 'pos_unique']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
         x = 0
         for d in testing_list:
-            writer.writerow({'text': text_bits[x], 'pos': perm_pos[x], 'total': list_nums[x], 'pos_unique': list_nums_unique[x]})
-            x += 1
+            if d.pos_ != "SPACE":
+                writer.writerow({'text': text_bits[x], 'pos': perm_pos[x], 'total': list_nums[x], 'pos_unique': list_nums_unique[x]})
+                x += 1
+        index_num += 1
